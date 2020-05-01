7aa39b08f993f8ef3968d773856aecad9c8c7ce2
Thiago Robles <roblesthiago38@gmail.com>
2020-05-01 16:42:17 -0300

    Added the  Sequelize ORM dependency, configured the folder structure of database

---
+++ api/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: '13.57.235.169',
+    port: '33356',
+    username: 'megahack',
+    password: '4dafba$2bff1@CFE204c0788773712D538',
+    database: 'megahackapp',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+};
---
+++ api/src/database/migrations/20200501184536-create-estabelecimentos.js
@@ -0,0 +1,84 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    
+      return queryInterface.createTable('users', { 
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+        },
+        nome: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        cpf_cnpj: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        telefone: {
+          type: Sequelize.INTEGER,
+          allowNull: false
+        },
+        sexo: {
+          type: Sequelize.STRING
+        },
+        tipo_pessoa: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        senha_hash: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        cep: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        endereco: {
+          type: Sequelize.String
+        },
+        numero: {
+          type: Sequelize.INTEGER
+        },
+        complemento: {
+          type: Sequelize.STRING
+        },
+        uf: {
+          type: Sequelize.STRING
+        },
+        categoria: {
+          type: Sequelize.STRING
+        },
+        atendimento_virtual: {
+          type: Sequelize.STRING
+        },
+        statu: {
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      });
+    
+  },
+
+  down: (queryInterface, Sequelize) => {
+    
+      return queryInterface.dropTable('estabelecimentos');
+    
+  }
+};
